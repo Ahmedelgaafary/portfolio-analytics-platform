3c29394fe795d26e988fb81ff220019aa4bebd35
Ahmedelgaafary <ahmedelg3fary88@gmail.com>
2026-07-23 11:46:48 +0300

    test(data): add cache and downloader tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,3 +3,31 @@
 
                         Part of Portfolio Analytics Platform.
                         """
+import pandas as pd
+
+from src.data_processing.cache import CacheManager
+
+
+def test_cache_save_load(tmp_path):
+
+    cache = CacheManager(tmp_path)
+
+    df = pd.DataFrame(
+        {
+            "A": [1, 2, 3]
+        }
+    )
+
+    cache.save(
+        "TEST",
+        df,
+    )
+
+    loaded = cache.load(
+        "TEST"
+    )
+
+    pd.testing.assert_frame_equal(
+        df,
+        loaded,
+    )
